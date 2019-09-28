70203847a1b7aafde59966a0c317e3cf0a37ce38
Mikalai_Klepchukou <Mikalai_Klepchukou@epam.com>
2019-09-28 17:00:47 +0300

    Fix logo and add Favorite

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,10 +5,10 @@ import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import BookmarksIcon from '@material-ui/icons/Bookmarks';
 import MapIcon from '@material-ui/icons/Map';
 import SwapCallsIcon from '@material-ui/icons/SwapCalls';
-import './Main.css';
-
 import { MapComponent } from '../Map/Map';
 import TripSelector from '../TripSelector';
+import { FavoriteComponent } from '../Favorite/Favorite';
+import './Main.css';
 
 const propTypes = {
     location: PropTypes.object,
@@ -16,24 +16,24 @@ const propTypes = {
 const defaultProps = {}
 
 const tileData = [
-	{
-		img: 'http://vandrouka.by/wp-content/uploads/2012/11/Mogilev.jpg',
-		name: 'Ratusha',
-		distance: 1250,
-		likes: 46
-	},
-	{
-		name: 'Maslennikov Museum',
-    img: 'http://photos.wikimapia.org/p/00/01/89/87/00_big.jpg',
-		distance: 390,
-		likes: 19,
-	},
-	{
-		name: 'The Skywatcher',
-    img: 'https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fbelarusfacts.by%2Fupload%2Fphotos%2Fphoto_about%2Fp7200004_1.jpg&f=1&nofb=1',
-		distance: 180,
-		likes: 28,
-	}
+    {
+        img: 'http://vandrouka.by/wp-content/uploads/2012/11/Mogilev.jpg',
+        name: 'Ratusha',
+        distance: 1250,
+        likes: 46
+    },
+    {
+        name: 'Maslennikov Museum',
+        img: 'http://photos.wikimapia.org/p/00/01/89/87/00_big.jpg',
+        distance: 390,
+        likes: 19,
+    },
+    {
+        name: 'The Skywatcher',
+        img: 'https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fbelarusfacts.by%2Fupload%2Fphotos%2Fphoto_about%2Fp7200004_1.jpg&f=1&nofb=1',
+        distance: 180,
+        likes: 28,
+    }
 ]
 
 export class MainComponent extends React.Component {
@@ -69,12 +69,12 @@ export class MainComponent extends React.Component {
                     {navigationTab === 'MAP' && (
                         <MapComponent tags={location.state.tags && location.state.tags.length > 0 ? location.state.tags : undefined} />
                     )}
-										{navigationTab === 'FAVORITE' && (
-												<TripSelector tripData={tileData} />
-										)}
-										{navigationTab === 'ROUTER' && (
-												<TripSelector tripData={tileData} />
-										)}
+                    {navigationTab === 'FAVORITE' && (
+                        <FavoriteComponent />
+                    )}
+                    {navigationTab === 'ROUTER' && (
+                        <TripSelector tripData={tileData} />
+                    )}
                 </div>
             </div >
         )
