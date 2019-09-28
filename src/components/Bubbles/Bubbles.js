0c48491fb137928e5b0c8cd4f68cf5aae2b71549
Andrei Ovchinnikov <kele6ra@gmail.com>
2019-09-28 15:16:30 +0300

    update home component

---
+++ src/components/Bubbles/Bubbles.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import BubbleChart from '@weknow/react-bubble-chart-d3';
+import './Bubbles.css';
+
+const propTypes = {}
+const defaultProps = {}
+
+export class BubblesComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        const tags = props.tags;
+        this.state = {
+            data: {},
+        }
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentDidUpdate() {
+        const data = this.props.tags.map((d) => ({
+            label: d.label,
+            value: d.label.length * 2,
+            color: d.state ? 'lightgreen' : 'lightblue'
+        }));
+
+        if (JSON.stringify(this.state.data) !== JSON.stringify(data)) {
+            this.setState({ data: data });
+        }
+    }
+
+    bubbleClick = (label) => {
+        let data = this.state.data;
+        data.map((d) => {
+            if (d.label === label) {
+                d.color = d.color === 'lightgreen' ? 'lightblue' : 'lightgreen';
+            }
+            return (d);
+        })
+        this.props.tagStateCallback(label);
+        this.setState({ data: data });
+    }
+
+    render() {
+
+        return (
+
+            <BubbleChart
+                graph={{
+                    zoom: 1,
+                    offsetX: -0.05,
+                    offsetY: -0.01,
+                }}
+                width={300}
+                height={300}
+                padding={0} // optional value, number that set the padding between bubbles
+                showLegend={false} // optional value, pass false to disable the legend.
+                labelFont={{
+                    family: 'Arial',
+                    size: 12,
+                    color: '#fff',
+                    weight: 'regular'
+                }}
+                bubbleClickFun={this.bubbleClick}
+                data={this.state.data}
+            />
+
+        )
+    }
+}
+
+BubblesComponent.defaultProps = defaultProps
+BubblesComponent.propTypes = propTypes
+BubblesComponent.displayName = 'BubblesComponent'
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,35 +1,115 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
-import Accessible from '@material-ui/icons/Accessible';
 import { app } from '../../firebase'
 import './Home.css';
-
+import logo from './logo.jpg';
+import { BubblesComponent } from '../Bubbles/Bubbles';
+import { Link } from 'react-router-dom'
 
 const propTypes = {}
 const defaultProps = {}
 
 export class HomeComponent extends React.Component {
+    state = {
+        language: "en",
+        tags: {},
+        redirect: false,
+        selsctedTags: []
+    }
 
-    componentDidMount() {
-        const firebaseRows = app.firestore().collection('test');
+    constructor(props) {
+        super(props);
+        const firebaseRows = app.firestore().collection('tags');
         firebaseRows.onSnapshot(snapshot => {
-            snapshot.docs.forEach(d => console.log('TTT', d.data()))
+            const res = snapshot.docs.map(d => d.data())[0];
+            if (this.state.language === "en") {
+                const tags = res.en.map((d) => {
+                    return ({ label: d, state: false })
+                })
+                this.setState({ tags: tags });
+            } else if (this.state.language === "ru") {
+                const tags = res.ru.map((d) => {
+                    return ({ label: d, state: false })
+                })
+                this.setState({ tags: tags });
+            }
         })
     }
 
+    componentDidMount() {
+
+    }
+
+    getLogoText = () => {
+        if (this.state.language === "en") {
+            return ('Choose the most interesrring place you want to visit:');
+        } else if (this.state.language === "ru") {
+            return ('Выберите места, которые вы хотите посетить:');
+        }
+    }
+
+    resetTags = () => {
+        let data = this.state.tags;
+        data.map((d) => {
+            d.state = false;
+            return (d);
+        })
+        this.setState({ tags: data });
+    }
+
+    sendTagsToMap = () => {
+        let selectedTags = [];
+        this.state.tags.forEach((d) => {
+            if (d.state === true) {
+                selectedTags.push(d.label);
+            }
+        });
+        this.setState({ selsctedTags: selectedTags });
+    }
+
+    updateTagsState = (label) => {
+        let data = this.state.tags;
+        let selectedTags = [];
+
+        data.map((d) => {
+            if (d.label === label) {
+                d.state = d.state ? false : true;
+                d.color = d.state ? 'lightgreen' : 'lightblue';
+            }
+            return (d);
+        });
+
+        this.state.tags.forEach((d) => {
+            if (d.state === true) {
+                selectedTags.push(d.label);
+            }
+        });
+
+        this.setState({ tags: data, selectedTags: selectedTags });
+    }
+
     render() {
+        const { tags } = this.state;
         return (
-            <div>
-                <Button variant="contained" color="primary">
-                    Hello World
-                </Button>
-                <Accessible />
+            <div className="home__wrap">
+                <div className="home__language">
+
+                </div>
+                <div className="home__logo">
+                    <div className="home__logoImgWrap"><img src={logo} alt={"logo"} className="home_logoImg" /></div>
+                    <div className="home__logoText">{this.getLogoText()} </div>
+                </div>
+                <div className="home__tags">
+                    <BubblesComponent tags={tags} tagStateCallback={this.updateTagsState}> </BubblesComponent>
+                </div>
+                <div className="home__buttons">
+                    <Button color="primary" onClick={this.resetTags}>Reset</Button>
+                    <Button color="primary"><Link to={{ pathname: '/map', state: { tags: this.state.selectedTags } }}>Next</Link></Button>
+                </div>
             </div>
         )
     }
 }
 
 HomeComponent.defaultProps = defaultProps
-HomeComponent.propTypes = propTypes
 HomeComponent.displayName = 'HomeComponent'
