3d26f8662cb4b3158c9fae7f8342b7819b3e7088
Andrei Ovchinnikov <kele6ra@gmail.com>
2019-09-28 17:11:50 +0300

    reset tags after button

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -61,7 +61,7 @@ export class HomeComponent extends React.Component {
             d.state = false;
             return (d);
         })
-        this.setState({ tags: data });
+        this.setState({ tags: data, selsctedTags: [] });
     }
 
     changeLang = () => {
@@ -99,7 +99,7 @@ export class HomeComponent extends React.Component {
                 selectedTags.push(this.state.allTags.en[i]);
             }
         });
-
+        console.log(selectedTags);
         this.setState({ tags: data, selectedTags: selectedTags });
     }
 
